502d1e118dfff184a258c65461cc10d489caa7a1
Karen McEwen <karenmcewen.engineer@gmail.com>
2018-06-12 22:39:18 -0400

    added notes and mylist example

--- listsndictionaries.py
+++ listsndictionaries.py
@@ -1,1 +1,16 @@
 # This is a program to practice using lists and dictionaries in Python
+
+# There are 4 types of collections:
+# List - ordered and changeable. Allows duplicate members. Use square brackets []
+# Tuple - ordered and unchangeable. Allows duplicate members. Use parenthesis ()
+# Set -  unordered and unindexed. No duplicate members. Use curly brackets {}
+# Dictionary - unordered, changeable and indexed. No duplicate members. Use curly brackets and colon{key:value}
+
+# List - ordered and changeable. Allows duplicate members. Use square brackets []
+# list elements do not all have to be the same type
+mylist = [1, "red", "true", 3.14159]
+print(mylist)
+# list indices start at 0
+print('The second element in the list is ' + mylist[1])
+print(mylist[0] + mylist[3])
+
