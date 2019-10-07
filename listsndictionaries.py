84f8bda040e31c31f75f7d9c58cc0bcc921fa7e5
Karen McEwen <karenmcewen.engineer@gmail.com>
2019-10-07 00:17:38 -0400

    list comprehension

--- listsndictionaries.py
+++ listsndictionaries.py
@@ -121,3 +121,7 @@ for value in range(1,11):
     cubes.append(value**3)
 
 print(cubes)
+
+# list comprehensions - allows you to combine elements into a single line of code
+squares = [value**2 for value in range(1,11)]
+print(squares)
